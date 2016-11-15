dc6b63478575fa40ff8b22ffface9ed66e0fc712
Juan Jose Ortega <jjortega@MacBook-Pro-de-Juan.local>
2016-11-15 15:04:30 +0100

    Add a new method to know how many free_nodes_exists

--- lib/nodes/grid_node_set.rb
+++ lib/nodes/grid_node_set.rb
@@ -12,6 +12,14 @@ module SeleniumGridConsoleParser
         hashObject["nodes"] = self.to_a.map { |o| Hash[o.to_hash.each_pair.to_a] }
       end
 
+      def free_nodes
+        free_nodes = GridNodeSet.new()
+        self.each do |node|
+          free_nodes.add(node) if node.free?
+        end
+        free_nodes
+      end
+
       def first_free_node
         self.each do |node|
           return node if node.free?
